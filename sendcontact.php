402c21421328f3369fd6bfbe755e82aac8caa70b
Alejandro Galaviz <alejandrogalavizsalazar@gmail.com>
2020-03-06 12:38:33 -0700

    cambios

--- sendcontact.php
+++ sendcontact.php
@@ -1,16 +1,22 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" ); //UPDATE THIS TO YOUR NAME
-define( "RECIPIENT_EMAIL", "john@example.com" ); //UPDATE THIS TO YOUR EMAIL ID
-define( "EMAIL_SUBJECT", "Website Visitor Message" ); //UPDATE THIS TO YOUR SUBJECT
+define( "RECIPIENT_NAME", "Desierto" ); //UPDATE THIS TO YOUR NAME
+define( "RECIPIENT_EMAIL", "snoriega@shugert.com.mx" ); //UPDATE THIS TO YOUR EMAIL ID
+define( "EMAIL_SUBJECT", "Reservación" ); //UPDATE THIS TO YOUR SUBJECT
 
 // Read the form values
 $success = false;
 $senderName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$senderPeople = isset( $_POST['people'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['people'] ) : "";
+$senderDate = isset( $_POST['date'] ) ? preg_replace( "", "", $_POST['date'] ) : "";
+$senderTime = isset( $_POST['time'] ) ? preg_replace( "", "", $_POST['time'] ) : "";
+$senderPhone = isset( $_POST['phone'] ) ? preg_replace( "", "", $_POST['phone'] ) : "";
+
 $original_message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-$message = 'Name: '.$senderName.'<br/>Email: '.$senderEmail.'<br/>Message: '.$original_message;
+$message = 'Nombre: '.$senderName.'<br/>Correo electronico: '.$senderEmail.'<br/>Telefono: '.$_POST['phone'].'<br/>Fecha: '.$_POST['date'].'<br/>Hora: '.$_POST['time'].'<br/>Personas: '.$_POST['people'].'<br/>Mensaje: '.$original_message;
+
 
 // If all values exist, send the email
 if ( $senderName && $senderEmail && $message ) {
@@ -25,13 +31,13 @@ if ( $success )
 {
 ?>
 	<script>
-		window.location='thanks.html';
+		window.location='gracias.html';
 	</script>
 <?php
 }
 else
 {
-	echo "<h1>There was a problem sending your message. Please try again.</h1>";
+	echo "<h1>Hubo un problema al enviar tu mensaje. Por favor intenta de nuevo</h1>";
 }
 ?>
 
